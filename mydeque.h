8a7d5b559456b0da16ef43190804efa6ae1ad31b
Kishan S Patel <bishan.batel@protonmail.com>
2024-10-16 20:48:45 -0700

    Completed

--- mydeque.h
+++ mydeque.h
@@ -4,57 +4,62 @@
 
 namespace DigiPen {
 
-    class Deque {
-        public:
-            // big 4
-            Deque();
-            Deque( Deque const& rhs );
-            Deque& operator=(Deque rhs);
-            ~Deque();
+  class Deque {
+  public:
+    // big 4
+    Deque();
 
-            // from array
-            Deque( int * array_, unsigned int size_ );
-            
-            // simple stuff
-            int   Size() const;
-            bool  Empty() const;
-            void  Clear();
-            int   Capacity() const;
+    Deque(Deque const &rhs);
 
-            // real stuff
-            void  Push_front(int val);
-            int   Pop_front();
-            void  Push_back(int val);
-            int   Pop_back();
-            int&  operator[](unsigned int pos) ;
-            int   operator[](unsigned int pos) const ;
+    Deque &operator=(Deque rhs);
 
-            // usefull stuff
-            void swap( Deque& other ) ;
+    ~Deque();
 
-            // non-standard stuff
-            Deque&    operator+=(const Deque& rhs);       // concatenate 2 deques
-            Deque     operator+(const Deque& rhs) const;  // concatenate 2 deques
-            Deque&    reverse() ;                // reverse in-place
-            Deque     operator~() const;                 // return new reverse
+    // from array
+    Deque(int *array_, unsigned int size_);
 
-            // implemented
-            void Print() const;
-        private:
-            // implemented
-            // NOP if new_capacity <= current size
-            void reallocate( int new_capacity );
-        private:
-            int b;    // index where deque's data starts
-            int e;    // index where deque's data ends
-            int size;
-            int capacity; // size of the array
-            int * array;  // data array
-    };
+    // simple stuff
+    int Size() const;
+    bool Empty() const;
+    void Clear();
+    int Capacity() const;
 
-    // non friend - but has to be declared in the header
-    std::ostream & operator<<(std::ostream & os, const Deque &d);
+    // real stuff
+    void Push_front(int val);
+    int Pop_front();
+    void Push_back(int val);
+    int Pop_back();
+    int &operator[](unsigned int pos);
+    int operator[](unsigned int pos) const;
 
-}
+    // usefull stuff
+    void swap(Deque &other);
+
+    // non-standard stuff
+    Deque &operator+=(const Deque &rhs); // concatenate 2 deques
+    Deque operator+(const Deque &rhs) const; // concatenate 2 deques
+    Deque &reverse(); // reverse in-place
+    Deque operator~() const; // return new reverse
+
+    // implemented
+    void Print() const;
+
+  private:
+    // implemented
+    // NOP if new_capacity <= current size
+    void reallocate(int new_capacity);
+
+  private:
+    int b; // index where deque's data starts
+    int e; // index where deque's data ends
+    int size;
+    int capacity; // size of the array
+    int *array; // data array
+  };
+
+  // non friend - but has to be declared in the header
+  std::ostream &operator<<(std::ostream &os, const Deque &d);
+
+} // namespace DigiPen
 
 #endif
